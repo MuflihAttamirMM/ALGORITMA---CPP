a599d1186063d623a27255c96e6b625d7981f8d3
Muflay <muflihattamirmuslim@gmail.com>
2026-03-06 15:37:08 +0700

    Void Input

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int a[20];
 int n;
 
-void input() {
+void input() 
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang Array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+    }
+        else
+    {
+            cout << "ERROR : Maksimum Panjang Array adalah 20\n";
     }
