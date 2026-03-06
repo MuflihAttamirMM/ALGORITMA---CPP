64a4c72fad4fb42df5ce9d29e42e21df7c59763c
Muflay <muflihattamirmuslim@gmail.com>
2026-03-06 15:38:05 +0700

    Void Display

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -22,3 +22,23 @@ void input()
     cout << "=====================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "=====================" << endl;
+    
+    for (int i = 0; i < n; i++) //i++ / i = i + 1
+    {
+        cout << "Masukkan Nilai Ke -" << (i + 1) << " : ";
+        cin >> a[i];
+    }
+}
+
+void display ()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen Array yang telah diurutkan" << endl;
+    cout << "=================================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data Ke -" << i + 1 << " : ";
+    }
+}
